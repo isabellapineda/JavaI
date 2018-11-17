62273d40fff3e3ea86bc9bd4b25f8798307aa8aa
isabella oineda <isabellapineda5@gmail.com>
2018-11-17 18:27:25 -0400

    clase 1 de jav

---
+++ Practica2Clase1/src/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Esta clase es de ejemplo
+ * @author Anggy_Rengifo 
+ */
+public class MainClass {
+    
+    public static void main (String[] args){
+        
+        // Este programa es para un Hola Mundo
+        System.out.println("Hola Mundo");
+    }
+    
+}
---
+++ clase1isabella/NetBeansProjects/clase1/src/MainClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *  Esta clase es de ejemplo
+ * @author Isabella Pineda
+ */
+public class MainClass {
+    public static void main(String[] args){
+        //este programa es de prueba
+        System.out.println("primera clase");
+    }
+    
+}
